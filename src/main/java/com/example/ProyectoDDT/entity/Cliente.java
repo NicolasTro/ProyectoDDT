9c77cc7708406699633e0d0418b0c7c13ae21814
Nicolas <nicolas@naussa.com.uy>
2024-07-07 19:17:52 +0200

    Se crea la entidad Cliente, incorporacion de notations

--- src/main/java/com/example/ProyectoDDT/entity/Cliente.java
+++ src/main/java/com/example/ProyectoDDT/entity/Cliente.java
@@ -1,18 +1,25 @@
 package com.example.ProyectoDDT.entity;
 
 
+import jakarta.persistence.*;
+import lombok.*;
 
-
-
+@Entity
+@Table(name="Clientes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer ID;
     private String nombre;
     private String pais;
     private String email;
     private Integer telefono;
     private String direccion;
-
-
 
 }
